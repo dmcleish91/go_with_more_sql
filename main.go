a232152522f7daf2660d6b0bb8622602d5b1dd61
Dwight Mcleish Jr <dmcleishjr@gmail.com>
2023-10-01 15:51:40 -0400

    feat: added echo endpoint to test sending response

--- main.go
+++ main.go
@@ -3,22 +3,54 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"os"
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
 )
 
 type User struct {
-	user_id           int
-	username          string
-	email             string
-	password          string
-	registration_date time.Time
+	User_id           int       `json:"id"`
+	Username          string    `json:"username"`
+	Email             string    `json:"email"`
+	Password          string    `json:"password"`
+	Registration_date time.Time `json:"registration_date"`
+}
+
+func init() {
+	fmt.Println("Starting the application...")
 }
 
 func main() {
+	e := echo.New()
+	e.GET("/users", getPerson)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func fetchAllUsers(conn *pgxpool.Pool) []User {
+	var users []User
+	rows, err := conn.Query(context.Background(), "SELECT * FROM users")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	for rows.Next() {
+		var user User
+		err := rows.Scan(&user.User_id, &user.Username, &user.Email, &user.Password, &user.Registration_date)
+		if err != nil {
+			fmt.Println(err)
+		}
+		users = append(users, user)
+	}
+
+	return users
+}
+
+func getPerson(c echo.Context) error {
 	godotenv.Load()
 	DATABASE_URL := os.Getenv("DATABASE_URL")
 
@@ -29,27 +61,15 @@ func main() {
 	}
 	defer conn.Close()
 
-	fetchAllUsers(conn)
-}
+	users := fetchAllUsers(conn)
 
-func fetchAllUsers(conn *pgxpool.Pool) {
-	var users []User
-	rows, err := conn.Query(context.Background(), "SELECT * FROM users")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
-		os.Exit(1)
+	user := User{
+		User_id:           users[0].User_id,
+		Username:          users[0].Username,
+		Email:             users[0].Email,
+		Password:          users[0].Password,
+		Registration_date: users[0].Registration_date,
 	}
 
-	for rows.Next() {
-		var user User
-		err := rows.Scan(&user.user_id, &user.username, &user.email, &user.password, &user.registration_date)
-		if err != nil {
-			fmt.Println(err)
-		}
-		users = append(users, user)
-	}
-
-	for _, v := range users {
-		fmt.Println(v, v.registration_date.GoString())
-	}
+	return c.JSON(http.StatusOK, user)
 }
